e7f56457dadcf5345ad2d3ea97177a3c26da2385
Harley Rayner <harleymteden@gmail.com>
2018-01-26 02:27:06 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence1)
   individual_words = sentence1.split
   individual_words.collect do |word|
-    word.reverse.join(" ,")
+    word.reverse
+    # individual_words.join(" ,")
   end
 end
